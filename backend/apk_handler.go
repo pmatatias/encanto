ae62a404960466cec91e4a4891aaf4d8cba59570
Matatias <matatias97@gmail.com>
2024-02-29 17:11:25 +0700

    apk handler enpoint

---
+++ backend/apk_handler.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
+)
+
+type EncantoApk struct {
+	gorm.Model
+	FileName  string `json:"fileName"`
+	ApkBase64 string `gorm:"-" json:"apkBase63"`
+	Version   string `json:"version"`
+}
+
+func ApkHandler(r *mux.Router, db *gorm.DB) {
+	r.HandleFunc("/api/version", func(w http.ResponseWriter, r *http.Request) {
+
+		apkFile := EncantoApk{}
+		dbResult := db.First(&apkFile)
+		if dbResult.Error != nil {
+			// handle error
+		}
+		fmt.Fprint(w, apkFile.Version)
+
+	}).Methods("GET")
+
+	r.HandleFunc("/api/download", func(w http.ResponseWriter, r *http.Request) {
+		apkInfo := EncantoApk{}
+
+		dbResult := db.First(&apkInfo, "id = ?", 1)
+		if dbResult.Error != nil {
+			w.WriteHeader(http.StatusNoContent)
+			fmt.Println(dbResult.Error)
+			return
+		}
+		fmt.Println("load file...")
+		tempFile, err := os.ReadFile("/files/latest.apk")
+		if err != nil {
+			fmt.Println(err)
+		}
+		base64File := base64.StdEncoding.EncodeToString(tempFile)
+		apkInfo.ApkBase64 = base64File
+
+		json.NewEncoder(w).Encode(&apkInfo)
+	}).Methods("GET")
+
+	r.HandleFunc("/api/upload", func(w http.ResponseWriter, r *http.Request) {
+		newApk := EncantoApk{}
+
+		err := json.NewDecoder(r.Body).Decode(&newApk)
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "Error devode Json: %v", err)
+			return
+		}
+
+		data, errBase := base64.StdEncoding.DecodeString(newApk.ApkBase64)
+		if errBase != nil {
+			log.Fatal("error", errBase)
+		}
+
+		os.WriteFile("/files/lates.apk", data, os.ModePerm)
+		db.Save(&newApk)
+		json.NewEncoder(w).Encode(newApk.Version)
+
+	}).Methods("POST")
+}
--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
 	fmt.Println("Test go")
+
+	// Update the connection string to match your MySQL database credentials
+	dsn := "root:@tcp(127.0.0.1:3306)/encanto?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database:", err)
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("failed to get generic database object:", err)
+	}
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+
+	// Migrate the schema
+	db.AutoMigrate(&EncantoApk{})
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Execute Hello world")
+		fmt.Fprint(w, "Hello World!")
+	}).Methods("GET")
+
+	ApkHandler(r, db)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+
 }
